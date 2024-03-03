f6971c19ab2f0a15a64f2b6eb70f93425ffd5841
Tomer Picker <>
2024-03-03 13:45:16 +0200

    Added initial project

---
+++ Authentication/TOTPUIKitExample/TOTP-demo-app-samples-iOS/TOTP-demo-app-samples-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TOTP-demo-app-samples-iOS
+//
+//  Created by Tomer Picker on 03/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
