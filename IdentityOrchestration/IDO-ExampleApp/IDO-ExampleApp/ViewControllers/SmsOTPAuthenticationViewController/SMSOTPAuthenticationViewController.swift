9f3effa3efd2fac9a1b54da09234dd183f2da5e3
Igor Babitski <igor.babitski@transmitsecurity.com>
2024-04-18 17:49:43 +0300

    Added support for SMS OTP action

--- IdentityOrchestration/IDO-ExampleApp/IDO-ExampleApp/ViewControllers/SmsOTPAuthenticationViewController/SMSOTPAuthenticationViewController.swift
+++ IdentityOrchestration/IDO-ExampleApp/IDO-ExampleApp/ViewControllers/SmsOTPAuthenticationViewController/SMSOTPAuthenticationViewController.swift
@@ -16,11 +16,36 @@ class SMSOTPAuthenticationViewController: BaseViewController {
     
     @IBOutlet weak var submitButton: UIButton!
     
+    @IBOutlet weak var otpInput: UITextField!
+    
     override func viewDidLoad() {
+        
+        initializeIdoSDK()
+        
         super.viewDidLoad()
 
         titleLabel.text = viewTitle ?? "SMS OTP Authentication"
         
         textLabel.text = viewDescription
+    }
+    
+    override func submitButtonTapped() {
+        guard let passcode = otpInput.text, !passcode.isEmpty else {
+            showAlert(title: "Incorrect OTP passcode", message: "The passcode should be a non-empty, numeric string.")
+            return
+        }
+        
+        super.submitButtonTapped()
+        
+        TSIdo.submitClientResponse(clientResponseOptionId: .clientInput, data: ["passcode": passcode])
+    }
+    
+    func initializeIdoSDK() {
+        do {
+            try TSIdo.initializeSDK()
+        } catch {
+            debugPrint("[DEBUG] Error: \(error)")
+            showAlert(title: "IDO SDK Initialization Error", message: "\(error)")
+        }
     }
 }
